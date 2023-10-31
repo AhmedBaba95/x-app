67d6f60f467a99f151d04f94694d26875867c03d
AhmedBaba95 <ahmedbabamohamed95@gmail.com>
2023-10-31 14:01:51 +0000

    last commit

--- theme.js
+++ theme.js
@@ -1,26 +1,34 @@
+// Wait for the document to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
+    // Get the theme button element
     const themeButton = document.getElementById("theme-button");
-    const body = document.body;
-    const result = document.querySelector(".result");
-    const operators = document.querySelector(".operators");
-    const equalityButton = document.querySelector(".equality button");
-    const numbers = document.querySelector(".numbers");
-    const placeholder = document.querySelector(".result input::placeholder")
-    const numbersRows = document.querySelectorAll(".numbers_row button")
+    
+    // Get references to various elements that should toggle the dark theme
+    const body = document.body;  // The <body> element
+    const result = document.querySelector(".result");  // The result section
+    const operators = document.querySelector(".operators");  // The operators section
+    const equalityButton = document.querySelector(".equality button");  // The equality button
+    const numbers = document.querySelector(".numbers");  // The numbers section
+    const placeholder = document.querySelector(".result input::placeholder");  // Placeholder for the input
+    const numbersRows = document.querySelectorAll(".numbers_row button");  // All number buttons
 
+    // Function to toggle the dark mode
     function toggleDarkMode() {
+        // Toggle the "dark-theme" class on different elements
         body.classList.toggle("dark-theme");
         themeButton.classList.toggle("dark-theme");
         result.classList.toggle("dark-theme");
         operators.classList.toggle("dark-theme");
         equalityButton.classList.toggle("dark-theme");
         placeholder.classList.toggle("dark-theme");
-        numbers.classList.toggle("dark-theme")
+        numbers.classList.toggle("dark-theme");
 
+        // Loop through number buttons and toggle "dark-theme" class for each
         numbersRows.forEach((button) => {
             button.classList.toggle("dark-theme");
         });
-        
+
+        // Update the theme button text
         if (body.classList.contains("dark-theme")) {
             themeButton.textContent = "Light mode";
         } else {
@@ -28,5 +36,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // Add an event listener to the theme button to trigger dark mode toggle
     themeButton.addEventListener("click", toggleDarkMode);
 });
